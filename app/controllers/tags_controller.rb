2d573efa1b61f0cd61775569b04d38560336cd6d
Burton Cooke <burtoncooke@yahoo.com>
2016-02-21 20:13:48 +0900

    Tagging feature complete

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,19 @@
+class TagsController < ApplicationController
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "You've deleted the tag '#{@tag.name}'!"
+    redirect_to tags_path
+  end
+
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_list = tags_string.split(",").collect {|el| el.strip.downcase}.uniq
+    new_or_found_tags = tag_list.collect {|name| Tag.find_or_create_by(name: name)}
+    self.tags = new_or_found_tags
+  end
 end
