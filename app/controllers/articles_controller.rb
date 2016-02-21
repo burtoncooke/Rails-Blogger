e5ddaa032c69487dc3de2efe05a7c51f9ea6cad3
Burton Cooke <burtoncooke@yahoo.com>
2016-02-21 10:53:37 +0900

    test push after adding public key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,12 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
+
 end
